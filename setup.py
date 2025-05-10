119ff138e9027d139d643c3d3a1f874cdf7666e2
Benoit <benoit.guchet@gmail.Com>
2025-05-10 20:29:59 +0200

    fix setup.py chemin module

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
+from setuptools import setup
+from setuptools.extension import Extension
+
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+import traceback
+import os
 
-setup(ext_modules = cythonize("samplerbox_src/samplerbox_audio.pyx"), include_dirs=[numpy.get_include()])
+extensions = cythonize(
+    "samplerbox_src/samplerbox_audio.pyx",
+    compiler_directives={"language_level": "3"},
+)
+
+output_dir = "SamplerBox/samplerbox_src"
+os.makedirs(output_dir, exist_ok=True)
+
+try:
+    setup(
+        name="samplerbox_audio",
+        ext_modules=extensions,
+        include_dirs=[numpy.get_include()],
+        zip_safe=False,
+    )
+except:
+    traceback.print_exc()
